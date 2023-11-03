2f6b680cf48ca6f39d62a2b45a45022fd0f293ff
Raymund Lapuz <raymund.lapuz.x@bulsu.edu.ph>
2023-11-04 00:38:21 +0800

    fix message

--- applicant/message.php
+++ applicant/message.php
@@ -1,58 +1,59 @@
   <style type="text/css">
-    .mailbox-controls .btn {
-      padding: 3px 8px;
-      margin: 0px 2px;
-    }
+.mailbox-controls .btn {
+    padding: 3px 8px;
+    margin: 0px 2px;
+}
   </style>
-<?php 
+  <?php 
 if (!isset($_GET['p'])) {
   # code... 
 ?>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">
-    
-        <!-- /.col -->
-        <div class="col-md-12">
-          <div class="box box-primary">
-            <div class="box-header with-border">
-              <h3 class="box-title">Messages</h3>
+      <!-- Main content -->
+      <section class="content">
+          <div class="row">
 
-              <div class="box-tools pull-right" style="margin-bottom: 5px;">
-                <div class="has-feedback">
-                  <input type="text" class="form-control input-sm" placeholder="Search Mail">
-                  <span class="fa fa-search form-control-feedback" style="margin-top: -25px"></span>
-                </div>
-              </div>
-              <!-- /.box-tools -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body no-padding">
-              <div class="mailbox-controls">
-                <!-- Check all button -->
-                <!-- <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i></button> -->
-                <div class="btn-group">
-                  <!-- <button type="button" class="btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button> -->
-              <!--     <button type="button" class="btn btn-default btn-sm"><i class="fa fa-reply"></i></button>
+              <!-- /.col -->
+              <div class="col-md-12">
+                  <div class="box box-primary">
+                      <div class="box-header with-border">
+                          <h3 class="box-title">Messages</h3>
+
+                          <div class="box-tools pull-right" style="margin-bottom: 5px;">
+                              <!-- <div class="has-feedback">
+                                  <input type="text" class="form-control input-sm" placeholder="Search Mail">
+                                  <span class="fa fa-search form-control-feedback" style="margin-top: -25px"></span>
+                              </div>a -->
+                          </div>
+                          <!-- /.box-tools -->
+                      </div>
+                      <!-- /.box-header -->
+                      <div class="box-body no-padding">
+                          <div class="mailbox-controls">
+                              <!-- Check all button -->
+                              <!-- <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i></button> -->
+                              <div class="btn-group">
+                                  <!-- <button type="button" class="btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button> -->
+                                  <!--     <button type="button" class="btn btn-default btn-sm"><i class="fa fa-reply"></i></button>
                   <button type="button" class="btn btn-default btn-sm"><i class="fa fa-share"></i></button> -->
-                </div>
-                <!-- /.btn-group -->
-                <!-- <button type="button" class="btn btn-default btn-sm"><i class="fa fa-refresh"></i></button> -->
-              <!--   <div class="pull-right">
+                              </div>
+                              <!-- /.btn-group -->
+                              <!-- <button type="button" class="btn btn-default btn-sm"><i
+                                      class="fa fa-refresh"></i></button> -->
+                              <!--   <div class="pull-right">
                   1-50/200
                   <div class="btn-group">
                     <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i></button>
                     <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-right"></i></button>
                   </div> 
                 </div> -->
-                <!-- /.pull-right -->
-              </div>
-              <div class="table-responsive mailbox-messages">
-                <table class="table table-hover table-striped">
-                  <tbody>
-                    <?php 
+                              <!-- /.pull-right -->
+                          </div>
+                          <div class="table-responsive mailbox-messages">
+                              <table class="table table-hover table-striped">
+                                  <tbody>
+                                      <?php 
                         $sql = "SELECT * FROM `tblcompany` c,`tbljobregistration` j,`tblfeedback` f  WHERE c.`COMPANYID`=j.`COMPANYID` AND j.`REGISTRATIONID`=f.`REGISTRATIONID` AND `PENDINGAPPLICATION`=0 AND j.`APPLICANTID`='{$_SESSION['APPLICANTID']}'";
                         $mydb->setQuery($sql);
                         $cur = $mydb->loadResultList();
@@ -66,47 +67,51 @@ if (!isset($_GET['p'])) {
                           echo '<td class="mailbox-date">'.$result->DATETIMEAPPROVED.'</td>';
                           echo '</tr>';
                         }
-                    ?> 
-                  </tbody>
-                </table>
-                <!-- /.table -->
-              </div>
-              <!-- /.mail-box-messages -->
-            </div>
-            <!-- /.box-body -->
-            <div class="box-footer no-padding">
-              <div class="mailbox-controls">
-                <!-- Check all button -->
-                <!-- <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i> -->
-                </button>
-                <div class="btn-group">
-                  <button type="button" class="btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button>
-                 <!--  <button type="button" class="btn btn-default btn-sm"><i class="fa fa-reply"></i></button>
+                    ?>
+                                  </tbody>
+                              </table>
+                              <!-- /.table -->
+                          </div>
+                          <!-- /.mail-box-messages -->
+                      </div>
+                      <!-- /.box-body -->
+                      <div class="box-footer no-padding">
+                          <div class="mailbox-controls">
+                              <!-- Check all button -->
+                              <!-- <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i> -->
+                              </button>
+                              <div class="btn-group">
+                                  <button type="button" class="btn btn-default btn-sm"><i
+                                          class="fa fa-trash-o"></i></button>
+                                  <!--  <button type="button" class="btn btn-default btn-sm"><i class="fa fa-reply"></i></button>
                   <button type="button" class="btn btn-default btn-sm"><i class="fa fa-share"></i></button> -->
-                </div>
-                <!-- /.btn-group -->
-                <a href="index.php?view=message" class="btn btn-default btn-sm"><i class="fa fa-refresh"></i></a>
-                <div class="pull-right">
-                  1-50/200
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i></button>
-                    <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-right"></i></button>
+                              </div>
+                              <!-- /.btn-group -->
+                              <a href="index.php?view=message" class="btn btn-default btn-sm"><i
+                                      class="fa fa-refresh"></i></a>
+                              <div class="pull-right">
+                                  1-10
+                                  <div class="btn-group">
+                                      <button type="button" class="btn btn-default btn-sm"><i
+                                              class="fa fa-chevron-left"></i></button>
+                                      <button type="button" class="btn btn-default btn-sm"><i
+                                              class="fa fa-chevron-right"></i></button>
+                                  </div>
+                                  <!-- /.btn-group -->
+                              </div>
+                              <!-- /.pull-right -->
+                          </div>
+                      </div>
                   </div>
-                  <!-- /.btn-group -->
-                </div>
-                <!-- /.pull-right -->
+                  <!-- /. box -->
               </div>
-            </div>
+              <!-- /.col -->
           </div>
-          <!-- /. box -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
+          <!-- /.row -->
+      </section>
+      <!-- /.content -->
   </div>
-   
- <?php }else{  
+
+  <?php }else{  
   require_once('readmessage.php');
  } ?>
--- process.php
+++ process.php
@@ -1,252 +1,242 @@
-<?php  
-require_once ("include/initialize.php");
+<?php
+// Include the necessary initialization file
+require_once("include/initialize.php");
+
+// Get the 'action' parameter from the URL or set it to an empty string if not present
 $action = (isset($_GET['action']) && $_GET['action'] != '') ? $_GET['action'] : '';
+
+// Perform different actions based on the 'action' parameter
 switch ($action) {
-	case 'submitapplication' :
-	doSubmitApplication();
-	break;
-  
-	case 'register' :
-	doRegister();
-	break;  
+    case 'submitapplication':
+        doSubmitApplication();
+        break;
 
-	case 'login' :
-	doLogin();
-	break; 
-	}
+    case 'register':
+        doRegister();
+        break;
 
-function doSubmitApplication() { 
-	global $mydb;   
-		$jobid  = $_GET['JOBID'];
-		
-
-		$autonum = New Autonumber();
-		$applicantid = $autonum->set_autonumber('APPLICANT');
-		$autonum = New Autonumber();
-		$fileid = $autonum->set_autonumber('FILEID');
-
-		@$picture = UploadImage();
-		@$location = "photos/". $picture ;
-
-
-		if ($picture=="") {
-			# code...
-			redirect(web_root."index.php?q=apply&job=".$jobid."&view=personalinfo");
-		}else{ 
-			
-			if (isset($_SESSION['APPLICANTID'])) {
-
-				$sql = "INSERT INTO `tblattachmentfile` (FILEID,`USERATTACHMENTID`, `FILE_NAME`, `FILE_LOCATION`, `JOBID`) 
-				VALUES ('". date('Y').$fileid->AUTO."','{$_SESSION['APPLICANTID']}','Resume','{$location}','{$jobid}')";
-				$mydb->setQuery($sql); 
-				$res = $mydb->executeQuery(); 
-
-				doUpdate($jobid,$fileid->AUTO);
-				
-			}else{
-				 
-				$sql = "INSERT INTO `tblattachmentfile` (FILEID,`USERATTACHMENTID`, `FILE_NAME`, `FILE_LOCATION`, `JOBID`) 
-				VALUES ('". date('Y').$fileid->AUTO."','". date('Y').$applicantid->AUTO."','Resume','{$location}','{$jobid}')";
-				// echo $sql;exit;
-				$mydb->setQuery($sql); 
-				$res = $mydb->executeQuery(); 
-
-				doInsert($jobid,$fileid->AUTO); 
-
-				$autonum = New Autonumber();
-				$autonum->auto_update('APPLICANT');
-			}
-		}
-
-		$autonum = New Autonumber();
-	    $autonum->auto_update('FILEID'); 
-	 
-}
-function doInsert($jobid=0,$fileid=0) {
-	if (isset($_POST['submit'])) {  
-	global $mydb; 
-
-			$birthdate =  $_POST['year'].'-'.$_POST['month'].'-'.$_POST['day'];
-
-			$age = date_diff(date_create($birthdate),date_create('today'))->y;
-
-			if ($age < 20){
-			message("Invalid age. 20 years old and above is allowed.", "error");
-			redirect("index.php?q=apply&view=personalinfo&job=".$jobid);
-
-			}else{
-
-			$autonum = New Autonumber();
-			$auto = $autonum->set_autonumber('APPLICANT');
-			 
-			$applicant =New Applicants();
-			$applicant->APPLICANTID = date('Y').$auto->AUTO;
-			$applicant->FNAME = $_POST['FNAME'];
-			$applicant->LNAME = $_POST['LNAME'];
-			$applicant->MNAME = $_POST['MNAME'];
-			$applicant->ADDRESS = $_POST['ADDRESS'];
-			$applicant->SEX = $_POST['optionsRadios'];
-			$applicant->CIVILSTATUS = $_POST['CIVILSTATUS'];
-			$applicant->BIRTHDATE = $birthdate;
-			$applicant->BIRTHPLACE = $_POST['BIRTHPLACE'];
-			$applicant->AGE = $age;
-			$applicant->USERNAME = $_POST['USERNAME'];
-			$applicant->PASS = sha1($_POST['PASS']);
-			$applicant->EMAILADDRESS = $_POST['EMAILADDRESS'];
-			$applicant->CONTACTNO = $_POST['TELNO'];
-			$applicant->DEGREE = $_POST['DEGREE'];
-			$applicant->create();
-
-
-			$sql = "SELECT * FROM `tblcompany` c,`tbljob` j WHERE c.`COMPANYID`=j.`COMPANYID` AND JOBID = '{$jobid}'" ;
-			$mydb->setQuery($sql);
-			$result = $mydb->loadSingleResult();
-
-
-			$jobreg = New JobRegistration(); 
-			$jobreg->COMPANYID = $result->COMPANYID;
-			$jobreg->JOBID     = $result->JOBID;
-			$jobreg->APPLICANTID = date('Y').$auto->AUTO;
-			$jobreg->APPLICANT   = $_POST['FNAME'] . ' ' . $_POST['LNAME'];
-			$jobreg->REGISTRATIONDATE = date('Y-m-d');
-			$jobreg->FILEID = date('Y').$fileid;
-			$jobreg->REMARKS = 'Pending';
-			$jobreg->DATETIMEAPPROVED = date('Y-m-d H:i');
-			$jobreg->create();
-  
-
-			message("Your application already submitted. Please wait for the company confirmation if your are qualified to this job.","success");
-			redirect("index.php?q=success&job=".$result->JOBID);
-
-			
-	 }
-}
-}
-function doUpdate($jobid=0,$fileid=0) {
-	if (isset($_POST['submit'])) {
-	global $mydb;   
-
-			$applicant =New Applicants();
-			$appl  = $applicant->single_applicant($_SESSION['APPLICANTID']);
-
-			 
-
-			$sql = "SELECT * FROM `tblcompany` c,`tbljob` j WHERE c.`COMPANYID`=j.`COMPANYID` AND JOBID = '{$jobid}'" ;
-			$mydb->setQuery($sql);
-			$result = $mydb->loadSingleResult();
-
-
-			$jobreg = New JobRegistration(); 
-			$jobreg->COMPANYID = $result->COMPANYID;
-			$jobreg->JOBID     = $result->JOBID;
-			$jobreg->APPLICANTID = $appl->APPLICANTID;
-			$jobreg->APPLICANT   = $appl->FNAME . ' ' . $appl->LNAME;
-			$jobreg->REGISTRATIONDATE = date('Y-m-d');
-			$jobreg->FILEID = date('Y').$fileid;
-			$jobreg->REMARKS = 'Pending';
-			$jobreg->DATETIMEAPPROVED = date('Y-m-d H:i');
-			$jobreg->create();
-
-  
-			message("Your application already submitted. Please wait for the company confirmation if your are qualified to this job.","success");
-			redirect("index.php?q=success&job=".$result->JOBID);
- 
-	}
-}
-function doRegister(){
-	global $mydb;
-	if (isset($_POST['btnRegister'])) { 
-			$birthdate =  $_POST['year'].'-'.$_POST['month'].'-'.$_POST['day'];
-
-			$age = date_diff(date_create($birthdate),date_create('today'))->y;
-
-			if ($age < 20){
-			message("Invalid age. 20 years old and above is allowed.", "error");
-			redirect("index.php?q=register");
-
-			}else{
-
-			$autonum = New Autonumber();
-			$auto = $autonum->set_autonumber('APPLICANT');
-			 
-			$applicant =New Applicants();
-			$applicant->APPLICANTID = date('Y').$auto->AUTO;
-			$applicant->FNAME = $_POST['FNAME'];
-			$applicant->LNAME = $_POST['LNAME'];
-			$applicant->MNAME = $_POST['MNAME'];
-			$applicant->ADDRESS = $_POST['ADDRESS'];
-			$applicant->SEX = $_POST['optionsRadios'];
-			$applicant->CIVILSTATUS = $_POST['CIVILSTATUS'];
-			$applicant->BIRTHDATE = $birthdate;
-			$applicant->BIRTHPLACE = $_POST['BIRTHPLACE'];
-			$applicant->AGE = $age;
-			$applicant->USERNAME = $_POST['USERNAME'];
-			$applicant->PASS = sha1($_POST['PASS']);
-			$applicant->EMAILADDRESS = $_POST['EMAILADDRESS'];
-			$applicant->CONTACTNO = $_POST['TELNO'];
-			$applicant->DEGREE = $_POST['DEGREE'];
-			$applicant->create();
-
-
- 
-			$autonum = New Autonumber();
-			$autonum->auto_update('APPLICANT');
-
-
-			message("You are successfully registered to the site. You can login now!","success");
-			redirect("index.php?q=success");
-
-			
-	 }
-}
+    case 'login':
+        doLogin();
+        break;
 }
 
-function doLogin(){
-	
-	$email = trim($_POST['USERNAME']);
-	$upass  = trim($_POST['PASS']);
-	$h_upass = sha1($upass);
- 
-  //it creates a new objects of member
+// Function to handle the submission of a job application
+function doSubmitApplication()
+{
+    global $mydb;
+
+    // Get the 'JOBID' from the URL
+    $jobid = $_GET['JOBID'];
+
+    // Generate unique IDs for the applicant and file
+    $autonum = new Autonumber();
+    $applicantid = $autonum->set_autonumber('APPLICANT');
+    $autonum = new Autonumber();
+    $fileid = $autonum->set_autonumber('FILEID');
+
+    // Upload an image and get its location
+    $picture = UploadImage();
+    $location = "photos/" . $picture;
+
+    if ($picture == "") {
+        // Redirect if no picture was uploaded
+        redirect(web_root . "index.php?q=apply&job=" . $jobid . "&view=personalinfo");
+    } else {
+        if (isset($_SESSION['APPLICANTID'])) {
+            // Insert data into the database if the user is already logged in
+            $sql = "INSERT INTO `tblattachmentfile` (FILEID,`USERATTACHMENTID`, `FILE_NAME`, `FILE_LOCATION`, `JOBID`) 
+                    VALUES ('" . date('Y') . $fileid->AUTO . "','{$_SESSION['APPLICANTID']}','Resume','{$location}','{$jobid}')";
+            $mydb->setQuery($sql);
+            $res = $mydb->executeQuery();
+
+            doUpdate($jobid, $fileid->AUTO);
+        } else {
+            // Insert data into the database for a new user
+            $sql = "INSERT INTO `tblattachmentfile` (FILEID,`USERATTACHMENTID`, `FILE_NAME`, `FILE_LOCATION`, `JOBID`) 
+                    VALUES ('" . date('Y') . $fileid->AUTO . "','" . date('Y') . $applicantid->AUTO . "','Resume','{$location}','{$jobid}')";
+            $mydb->setQuery($sql);
+            $res = $mydb->executeQuery();
+
+            doInsert($jobid, $fileid->AUTO);
+
+            $autonum = new Autonumber();
+            $autonum->auto_update('APPLICANT');
+        }
+    }
+
+    $autonum = new Autonumber();
+    $autonum->auto_update('FILEID');
+}
+
+// Function to handle the insertion of applicant data
+function doInsert($jobid = 0, $fileid = 0)
+{
+    if (isset($_POST['submit'])) {
+        global $mydb;
+
+        // Calculate the age based on the birthdate
+        $birthdate = $_POST['year'] . '-' . $_POST['month'] . '-' . $_POST['day'];
+        $age = date_diff(date_create($birthdate), date_create('today'))->y;
+
+        if ($age < 20) {
+            message("Invalid age. 20 years old and above is allowed.", "error");
+            redirect("index.php?q=apply&view=personalinfo&job=" . $jobid);
+        } else {
+            $autonum = new Autonumber();
+            $auto = $autonum->set_autonumber('APPLICANT');
+
+            $applicant = new Applicants();
+            $applicant->APPLICANTID = date('Y') . $auto->AUTO;
+            $applicant->FNAME = $_POST['FNAME'];
+            $applicant->LNAME = $_POST['LNAME'];
+            $applicant->MNAME = $_POST['MNAME'];
+            $applicant->ADDRESS = $_POST['ADDRESS'];
+            $applicant->SEX = $_POST['optionsRadios'];
+            $applicant->CIVILSTATUS = $_POST['CIVILSTATUS'];
+            $applicant->BIRTHDATE = $birthdate;
+            $applicant->BIRTHPLACE = $_POST['BIRTHPLACE'];
+            $applicant->AGE = $age;
+            $applicant->USERNAME = $_POST['USERNAME'];
+            $applicant->PASS = sha1($_POST['PASS']);
+            $applicant->EMAILADDRESS = $_POST['EMAILADDRESS'];
+            $applicant->CONTACTNO = $_POST['TELNO'];
+            $applicant->DEGREE = $_POST['DEGREE'];
+            $applicant->create();
+
+            // Retrieve company and job information
+            $sql = "SELECT * FROM `tblcompany` c,`tbljob` j WHERE c.`COMPANYID`=j.`COMPANYID` AND JOBID = '{$jobid}'";
+            $mydb->setQuery($sql);
+            $result = $mydb->loadSingleResult();
+
+            $jobreg = new JobRegistration();
+            $jobreg->COMPANYID = $result->COMPANYID;
+            $jobreg->JOBID = $result->JOBID;
+            $jobreg->APPLICANTID = date('Y') . $auto->AUTO;
+            $jobreg->APPLICANT = $_POST['FNAME'] . ' ' . $_POST['LNAME'];
+            $jobreg->REGISTRATIONDATE = date('Y-m-d');
+            $jobreg->FILEID = date('Y') . $fileid;
+            $jobreg->REMARKS = 'Pending';
+            $jobreg->DATETIMEAPPROVED = date('Y-m-d H:i');
+            $jobreg->create();
+
+            message("Your application already submitted. Please wait for the company confirmation if you are qualified for this job.", "success");
+            redirect("index.php?q=success&job=" . $result->JOBID);
+        }
+    }
+}
+
+// Function to handle the update of applicant data
+function doUpdate($jobid = 0, $fileid = 0)
+{
+    if (isset($_POST['submit'])) {
+        global $mydb;
+
+        $applicant = new Applicants();
+        $appl = $applicant->single_applicant($_SESSION['APPLICANTID']);
+
+        // Retrieve company and job information
+        $sql = "SELECT * FROM `tblcompany` c,`tbljob` j WHERE c.`COMPANYID`=j.`COMPANYID` AND JOBID = '{$jobid}'";
+        $mydb->setQuery($sql);
+        $result = $mydb->loadSingleResult();
+
+        $jobreg = new JobRegistration();
+        $jobreg->COMPANYID = $result->COMPANYID;
+        $jobreg->JOBID = $result->JOBID;
+        $jobreg->APPLICANTID = $appl->APPLICANTID;
+        $jobreg->APPLICANT = $appl->FNAME . ' ' . $appl->LNAME;
+        $jobreg->REGISTRATIONDATE = date('Y-m-d');
+        $jobreg->FILEID = date('Y') . $fileid;
+        $jobreg->REMARKS = 'Pending';
+        $jobreg->DATETIMEAPPROVED = date('Y-m-d H:i');
+        $jobreg->create();
+
+        message("Your application already submitted. Please wait for the company confirmation if you are qualified for this job.", "success");
+        redirect("index.php?q=success&job=" . $result->JOBID);
+    }
+}
+
+// Function to handle user registration
+function doRegister()
+{
+    global $mydb;
+    if (isset($_POST['btnRegister'])) {
+        // Calculate the age based on the birthdate
+        $birthdate = $_POST['year'] . '-' . $_POST['month'] . '-' . $_POST['day'];
+        $age = date_diff(date_create($birthdate), date_create('today'))->y;
+
+        if ($age < 20) {
+            message("Invalid age. 20 years old and above is allowed.", "error");
+            redirect("index.php?q=register");
+        } else {
+            $autonum = new Autonumber();
+            $auto = $autonum->set_autonumber('APPLICANT');
+
+			// this error undefined is for readable code not actually an error
+            $applicant = new Applicants();
+            $applicant->APPLICANTID = date('Y') . $auto->AUTO;
+            $applicant->FNAME = $_POST['FNAME'];
+            $applicant->LNAME = $_POST['LNAME'];
+            $applicant->MNAME = $_POST['MNAME'];
+            $applicant->ADDRESS = $_POST['ADDRESS'];
+            $applicant->SEX = $_POST['optionsRadios'];
+            $applicant->CIVILSTATUS = $_POST['CIVILSTATUS'];
+            $applicant->BIRTHDATE = $birthdate;
+            $applicant->BIRTHPLACE = $_POST['BIRTHPLACE'];
+            $applicant->AGE = $age;
+            $applicant->USERNAME = $_POST['USERNAME'];
+            $applicant->PASS = sha1($_POST['PASS']);
+            $applicant->EMAILADDRESS = $_POST['EMAILADDRESS'];
+            $applicant->CONTACTNO = $_POST['TELNO'];
+            $applicant->DEGREE = $_POST['DEGREE'];
+            $applicant->create();
+
+            $autonum = new Autonumber();
+            $autonum->auto_update('APPLICANT');
+
+            message("You are successfully registered to the site. You can login now!", "success");
+            redirect("index.php?q=success");
+        }
+    }
+}
+
+// Function to handle user login
+function doLogin()
+{
+    $email = trim($_POST['USERNAME']);
+    $upass = trim($_POST['PASS']);
+    $h_upass = sha1($upass);
+
+    // Create an object of the Applicants class
     $applicant = new Applicants();
-    //make use of the static function, and we passed to parameters
+
+    // Attempt to authenticate the user
     $res = $applicant->applicantAuthentication($email, $h_upass);
-    if ($res==true) { 
 
-       	message("You are now successfully login!","success");
-       
-       // $sql="INSERT INTO `tbllogs` (`USERID`,USERNAME, `LOGDATETIME`, `LOGROLE`, `LOGMODE`) 
-       //    VALUES (".$_SESSION['USERID'].",'".$_SESSION['FULLNAME']."','".date('Y-m-d H:i:s')."','".$_SESSION['UROLE']."','Logged in')";
-       //    mysql_query($sql) or die(mysql_error()); 
-         redirect(web_root."applicant/");
-     
-    }else{
-    	 echo "Account does not exist! Please contact Administrator."; 
-    } 
+    if ($res == true) {
+        message("You are now successfully login!", "success");
+        redirect(web_root . "applicant/");
+    } else {
+        echo "Account does not exist! Please contact Administrator.";
+    }
 }
- 
-function UploadImage($jobid=0){
-	$target_dir = "applicant/photos/";
-	$target_file = $target_dir . date("dmYhis") . basename($_FILES["picture"]["name"]);
-	$uploadOk = 1;
-	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-	
-	
-	if($imageFileType != "jpg" || $imageFileType != "png" || $imageFileType != "jpeg"
-|| $imageFileType != "gif" ) {
-		 if (move_uploaded_file($_FILES["picture"]["tmp_name"], $target_file)) {
-			return  date("dmYhis") . basename($_FILES["picture"]["name"]);
-		}else{
-			message("Error Uploading File","error");
-			// redirect(web_root."index.php?q=apply&job=".$jobid."&view=personalinfo");
-			// exit;
-		}
-	}else{
-			message("File Not Supported","error");
-			// redirect(web_root."index.php?q=apply&job=".$jobid."&view=personalinfo");
-			// exit;
-		}
-} 
 
+// Function to upload an image
+function UploadImage($jobid = 0)
+{
+    $target_dir = "applicant/photos/";
+    $target_file = $target_dir . date("dmYhis") . basename($_FILES["picture"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
+    if ($imageFileType != "jpg" || $imageFileType != "png" || $imageFileType != "jpeg"
+        || $imageFileType != "gif") {
+        if (move_uploaded_file($_FILES["picture"]["tmp_name"], $target_file)) {
+            return date("dmYhis") . basename($_FILES["picture"]["name"]);
+        } else {
+            message("Error Uploading File", "error");
+        }
+    } else {
+        message("File Not Supported", "error");
+    }
+}
 ?>
